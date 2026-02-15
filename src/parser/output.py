df9255a90dfea2c41a5e65264aba1a7ad4be2507
Ivan Chernykh <ivanchernykh98@gmail.com>
2026-02-15 19:33:01 +0200

    fix: sorting

--- src/parser/output.py
+++ src/parser/output.py
@@ -5,6 +5,14 @@ from pathlib import Path
 from utils.args import ARG_LIMIT, ARG_PATH
 
 
+def sort_vacancies(recommended_vacancies: list[dict]):
+    return sorted(
+        recommended_vacancies,
+        key=lambda x: x["score"],
+        reverse=True,
+    )[:ARG_LIMIT]
+
+
 def save_to_json(data: dict, recommended_vacancies: list[dict]):
     filename = f"djinni_{datetime.today().strftime('%d%m%y_%H%M%S')}.json"
     output_dir = Path(ARG_PATH)
@@ -19,11 +27,7 @@ def save_to_json(data: dict, recommended_vacancies: list[dict]):
         json.dump(
             {
                 **data,
-                "recommended_vacancies": sorted(
-                    recommended_vacancies,
-                    key=lambda x: x["score"],
-                    reverse=True,
-                )[:ARG_LIMIT],
+                "recommended_vacancies": sort_vacancies(recommended_vacancies),
             },
             f,
             ensure_ascii=False,
@@ -32,7 +36,9 @@ def save_to_json(data: dict, recommended_vacancies: list[dict]):
 
 
 def print_result(jobs_total: int, recommended_vacancies: list[dict]):
+    sorted_vacancies = sort_vacancies(recommended_vacancies)
+
     print(f"[+] Total jobs: {jobs_total}\n")
-    for idx, job in enumerate(recommended_vacancies, 1):
+    for idx, job in enumerate(sorted_vacancies, 1):
         print(f"[+] {job['title']}")
         print(f"    {job['link']}\n")
