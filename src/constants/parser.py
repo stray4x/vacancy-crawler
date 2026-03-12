4d5b3311ca666e215d8916b6c95dc08d909c560e
Ivan Chernykh <ivanchernykh98@gmail.com>
2026-03-12 13:49:36 +0200

    fixed selectors for new ui

--- src/constants/parser.py
+++ src/constants/parser.py
@@ -2,7 +2,7 @@ PAGE_SIZE = 15
 
 VACANCY_LIST_SELECTORS = {
     "jobs_total": "header div span",
-    "job_item": "li[id^='job-item']",
-    "job_link": "a.job-item__title-link",
+    "job_item": "[id^='job-item']",
+    "job_link": "a.job_item__header-link",
     "job_desc": "[id^='job-description']",
 }
